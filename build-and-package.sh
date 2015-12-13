c093fa6b909083b1e8a905772016e8dbb72f9489
Callum Rogers <callumrogers@hotmail.co.uk>
2015-12-13 13:15:26 +0000

    Output ghc-head to the right place

--- build-and-package.sh
+++ build-and-package.sh
@@ -4,11 +4,11 @@ set -eu
 
 docker build -t ghc-builder build/
 
-if [[ -e ghc-head ]]; then
+if [[ -e package/ghc-head ]]; then
   echo Using cached ghc-head
 else
   echo Rebuilding ghc
-  docker run -it -v "$(pwd)/ghc-head:/opt/ghc/head" ghc-builder /build-ghc.sh
+  docker run -it -v "$(pwd)/package/ghc-head:/opt/ghc/head" ghc-builder /build-ghc.sh
 fi
 
 docker build -t packaged-ghc-head package/
